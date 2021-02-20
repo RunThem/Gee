4f6d08c5d2b0e41bc83e1b7c9a72f205fb6aea9e
iccy <iccy.fun@outlook.com>
2021-02-21 02:41:02 +0800

    test(_example/): example

--- _example/go.mod
+++ _example/go.mod
@@ -1,3 +1,5 @@
 module example
 
 go 1.14
+
+require github.com/RunThem/gee v0.0.0-20210220181107-323f0658d62a
--- _example/main.go
+++ _example/main.go
@@ -4,27 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/RunThem/gee"
 )
 
-// This is a simple example of a net/http package
+// This is a simple example of a gee package
 func main() {
-	engine := new(Engine)
-	log.Fatal(http.ListenAndServe(":8080", engine))
-}
+	r := gee.New()
 
-// Implementing the Handler interface
-type Engine struct{}
-
-// The method required for Handler
-func (e *Engine) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
+	r.GET("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, "URL.Path = %q\n", r.URL.Path)
-	case "/hello":
-		for k, v := range r.Header {
+	})
+
+	r.GET("/hello", func(rw http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header{
 			fmt.Fprintf(rw, "Header[%q] = %q\n", k, v)
 		}
-	default:
-		fmt.Fprintf(rw, "404 not found: %s\n", r.URL)
-	}
+	})
+
+	fmt.Println("Start ...")
+	log.Println(r.Run(":8080"))
 }
